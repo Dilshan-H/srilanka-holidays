e4efc94d5116b05325960d5cdecd764a24c6cd7b
dilshan-h <77499497+Dilshan-H@users.noreply.github.com>
2023-11-15 16:15:10 +0530

    Debug stage checks

--- converters/icalendar_to_csv.py
+++ converters/icalendar_to_csv.py
@@ -15,12 +15,14 @@ def ics_to_csv(file_path):
 
     csv_dir = os.path.abspath(os.path.join(os.curdir, os.pardir, CSV_DIR_NAME))
     os.makedirs(csv_dir, exist_ok=True)
+    print(f"Created directory: {csv_dir}")
 
     # Get the filename (without extension) from the input path
     file_name = os.path.splitext(os.path.basename(file_path))[0]
 
     # Create the CSV file path
     csv_file_path = os.path.join(csv_dir, f"{file_name}.csv")
+    print(f"CSV file path: {csv_file_path}")
 
     with open(csv_file_path, "w", newline="", encoding="utf-8") as file:
         fieldnames = [
