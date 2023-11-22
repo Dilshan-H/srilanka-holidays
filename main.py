9c7442a4ef357d6d65a1a4ed943fe3a2678dfe30
dilshan-h <77499497+Dilshan-H@users.noreply.github.com>
2023-11-22 23:54:41 +0530

    Refactor code

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
 # pylint: disable=missing-docstring import-error
-
-from typing import Union
-
+from typing import Annotated
+import os
 from datetime import datetime, date
 import json
-from fastapi import FastAPI, Response, status
-from pydantic import BaseModel
+from fastapi import FastAPI, Response, status, Path, Query
+
 
 app = FastAPI()
-
-
-class Item(BaseModel):
-    name: str
-    id: float
-    is_holiday: Union[bool, None] = None
 
 
 @app.get("/")
@@ -36,98 +29,99 @@ async def api_version():
 
 
 @app.get("/api/v1/coverage/{year}")
-async def api_coverage_year(year: int):
+async def api_coverage_year(
+    year: Annotated[int, Path(title="The year to be checked", ge=2000, le=3000)]
+):
     """Return current data coverage in the API for a given year"""
+    filename = f"json/{year}.json"
+    # check if data exists
+    if os.path.isfile(filename):
+        return {
+            "year": year,
+            "coverage": "ok",
+        }
+
     return {
-        "coverage": "ok",
         "year": year,
-        "total_holidays": 10,  # TODO: calculate number
-        "last_updated": "2023-01-01",
-    }  # TODO: change date
+        "coverage": "data not available",
+    }
 
 
 @app.get("/api/v1/check_holiday")
-async def check_holiday(year: int, month: int, day: int, response: Response):
+async def check_holiday(
+    year: Annotated[int, Query(ge=2000, le=3000)],
+    month: Annotated[int, Query(ge=1, le=12)],
+    day: Annotated[int, Query(ge=1, le=31)],
+    response: Response,
+):
     """Return whether a given date is a holiday or not"""
-    try:
-        date_to_check = date(year, month, day)
-    except ValueError:
-        response.status_code = status.HTTP_400_BAD_REQUEST
-        return {"error": "invalid date"}
-    except TypeError:
-        response.status_code = status.HTTP_400_BAD_REQUEST
-        return {"error": "invalid date format"}
-
-    # open corresponding json file and check for date
-    filename = f"json/{year}.json"
-    try:
-        with open(filename, "r", encoding="utf-8") as file:
-            holiday_data = json.load(file)
-    except FileNotFoundError:
-        response.status_code = status.HTTP_404_NOT_FOUND
-        return {"date": date_to_check, "error": "requested year not available"}
-
-    for holiday in holiday_data:
-        start_date = datetime.strptime(holiday["start"], "%Y-%m-%d").date()
-        end_date = datetime.strptime(holiday["end"], "%Y-%m-%d").date()
-
-        if start_date <= date_to_check < end_date:
-            response.status_code = status.HTTP_200_OK
-            return {
-                "date": date_to_check,
-                "response": True,
-            }
-
-    response.status_code = status.HTTP_200_OK
-    return {"date": date_to_check, "response": False}
+    date_provided, status_code, result = await get_holiday_info(year, month, day)
+    if response:
+        response.status_code = status_code
+    if result["is_holiday"]:
+        return {"date": date_provided, "response": True}
+    return {"date": date_provided, "response": False}
 
 
 @app.get("/api/v1/holiday_info")
-async def holiday_info(year: int, month: int, day: int, response: Response):
+async def holiday_info(
+    year: Annotated[int, Query(ge=2000, le=3000)],
+    month: Annotated[int, Query(ge=1, le=12)],
+    day: Annotated[int, Query(ge=1, le=31)],
+    response: Response,
+):
     """Return information about a given holiday"""
+    date_provided, status_code, result = await get_holiday_info(year, month, day)
+    if response:
+        response.status_code = status_code
+    return {"date": date_provided, "response": result}
+
+
+async def get_holiday_info(year: int, month: int, day: int):
     try:
         date_to_check = date(year, month, day)
-    except ValueError:
-        response.status_code = status.HTTP_400_BAD_REQUEST
-        return {"error": "invalid date"}
-    except TypeError:
-        response.status_code = status.HTTP_400_BAD_REQUEST
-        return {"error": "invalid date format"}
+    except (ValueError, TypeError):
+        return None, status.HTTP_400_BAD_REQUEST, {"error": "invalid date"}
 
-    # open corresponding json file and check for date
     filename = f"json/{year}.json"
     try:
         with open(filename, "r", encoding="utf-8") as file:
             holiday_data = json.load(file)
     except FileNotFoundError:
-        response.status_code = status.HTTP_404_NOT_FOUND
-        return {"date": date_to_check, "error": "requested year not available"}
+        return (
+            date_to_check,
+            status.HTTP_404_NOT_FOUND,
+            {"error": "requested year not available"},
+        )
 
     for holiday in holiday_data:
         start_date = datetime.strptime(holiday["start"], "%Y-%m-%d").date()
         end_date = datetime.strptime(holiday["end"], "%Y-%m-%d").date()
 
         if start_date <= date_to_check < end_date:
-            response.status_code = status.HTTP_200_OK
-            return {
-                "date": date_to_check,
-                "day": date_to_check.strftime("%A"),
-                "week": date_to_check.strftime("%W"),
-                "month": date_to_check.strftime("%B"),
-                "is_holiday": True,
-                "id": holiday["uid"],
-                "holiday": holiday["summary"],
-                "type": holiday["catagories"],
-                "holiday_start": holiday["start"],
-                "holiday_end": holiday["end"],
-            }
+            return (
+                date_to_check,
+                status.HTTP_200_OK,
+                {
+                    "day": date_to_check.strftime("%A"),
+                    "week": date_to_check.strftime("%W"),
+                    "month": date_to_check.strftime("%B"),
+                    "is_holiday": True,
+                    "id": holiday["uid"],
+                    "holiday": holiday["summary"],
+                    "type": holiday["catagories"],
+                    "holiday_start": holiday["start"],
+                    "holiday_end": holiday["end"],
+                },
+            )
 
-    response.status_code = status.HTTP_200_OK
-    return {
-        "date": date_to_check,
-        "is_holiday": False,
-        "error": "date is not a holiday",
-    }
+    return (
+        date_to_check,
+        status.HTTP_200_OK,
+        {
+            "is_holiday": False,
+        },
+    )
 
 
 @app.get("/api/v1/holidays")
@@ -143,5 +137,5 @@ async def read_item(item_id: int):
 
 
 @app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
+def update_item(item_id: int, item: int):
     return {"item_name": item.name, "item_id": item_id}
